f1a4d446054b2561160d2b63122d1fe6aa22db4b
Indika Maligaspe <k.indika.maligaspe@gmail.com>
2024-06-26 06:47:28 +0400

    initial commit

---
+++ js/findAnElementInUnsortedArray.js
@@ -0,0 +1,11 @@
+// Given a unsroted array [3,6,13,10,1,2,15,11] , find a specific element "13"
+
+function quickSort(arr){
+
+}
+
+function search(arr, target){
+
+}
+
+console.log(search([3,6,13,10,1,2,15,11], 13))
