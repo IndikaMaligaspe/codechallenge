877a66abb2a36ac9813c6bccfe149fc736107246
Indika Maligaspe <k.indika.maligaspe@gmail.com>
2024-06-26 07:13:09 +0400

    added insertion sort

---
+++ js/sortAnArrayWithInsertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr){
+    if(arr.length <= 1) return arr;
+    for(let i = 0; i< arr.length; i++) {
+        for(let j = i + 1; j <= arr.length; j++) {
+            if ( arr[j] < arr[i] ) {
+               let temp = arr[i];
+               arr[i] = arr[j]
+               arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(insertionSort([5,1,3,10,2,3,12,11]));
